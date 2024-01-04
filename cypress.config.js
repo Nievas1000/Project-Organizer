ec22dec5bf34b8a36ceba2406653f2f7166cbe93
Lautaro Nievas <lautynievas09@gmail.com>
2024-01-04 16:31:22 -0300

    job to tests in ci added

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
 /* eslint-disable */
 import { defineConfig } from "cypress";
-import { config } from "dotenv";
-config();
 
 export default defineConfig({
-  env: {
-    apiBaseUrl: process.env.VITE_API_BASE_URL,
-  },
   chromeWebSecurity: false,
   e2e: {
     setupNodeEvents(on, config) {
